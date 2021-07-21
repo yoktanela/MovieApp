8f346922e34da58537cee001612eda9428057868
yoktanela <elanuryoktan@gmail.com>
2021-07-21 05:40:07 +0300

    Display movie credits

--- MovieApp/MovieApp/ViewControllers/PersonDetailViewController.swift
+++ MovieApp/MovieApp/ViewControllers/PersonDetailViewController.swift
@@ -37,6 +37,18 @@ class PersonDetailViewController: UIViewController {
         return textView
     }()
     
+    public let movieCreditsLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = UIColor.init(netHex: 0x707070)
+        label.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+        label.textAlignment = .left
+        label.text = "Movie Credits"
+        return label
+    }()
+    
+    private var moviesCollectionView: UICollectionView!
+    private var castDataSource: CollectionViewDataSource<CastMemberCollectionViewCell,Movie>!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.white
@@ -61,10 +73,35 @@ class PersonDetailViewController: UIViewController {
         self.view.addSubview(biographyTextView)
         biographyTextView.translatesAutoresizingMaskIntoConstraints = false
         let biographyTextViewTop = biographyTextView.topAnchor.constraint(equalTo: biographyLabel.bottomAnchor, constant: 5)
-        let biographyTextViewBottom = biographyTextView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -16)
         let biographyTextViewLeft = biographyTextView.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 16)
         let biographyTextViewRight = biographyTextView.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: -16)
-        self.view.addConstraints([biographyTextViewTop, biographyTextViewLeft, biographyTextViewRight,biographyTextViewBottom])
+        self.view.addConstraints([biographyTextViewTop, biographyTextViewLeft, biographyTextViewRight])
+        
+        // movieCreditsLabel constraints
+        self.view.addSubview(movieCreditsLabel)
+        movieCreditsLabel.translatesAutoresizingMaskIntoConstraints = false
+        let movieCreditsLabelTop = movieCreditsLabel.topAnchor.constraint(equalTo: biographyTextView.bottomAnchor, constant: 10)
+        let movieCreditsLabelLeft = movieCreditsLabel.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 10)
+        let movieCreditsLabelRight = movieCreditsLabel.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: 10)
+        self.view.addConstraints([movieCreditsLabelTop, movieCreditsLabelLeft, movieCreditsLabelRight])
+        
+        // moviesCollectionView constraints
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+               layout.sectionInset = UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 10)
+        layout.itemSize = CGSize(width: 180, height: 55)
+        self.moviesCollectionView = UICollectionView(frame: CGRect.zero, collectionViewLayout: layout)
+        layout.scrollDirection = .horizontal
+        moviesCollectionView.backgroundColor = UIColor.white
+        moviesCollectionView.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(moviesCollectionView)
+        moviesCollectionView.delegate = self
+        let moviesCollectionViewTop = moviesCollectionView.topAnchor.constraint(equalTo: movieCreditsLabel.bottomAnchor, constant: 10)
+        let moviesCollectionViewBottom = moviesCollectionView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -20)
+        let moviesCollectionViewLeft = moviesCollectionView.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 10)
+        let moviesCollectionViewRight = moviesCollectionView.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: 0)
+        let moviesCollectionViewHeight = moviesCollectionView.heightAnchor.constraint(equalToConstant: 65)
+        self.view.addConstraints([moviesCollectionViewTop, moviesCollectionViewBottom, moviesCollectionViewLeft, moviesCollectionViewRight, moviesCollectionViewHeight])
+        moviesCollectionView.register(CastMemberCollectionViewCell.self, forCellWithReuseIdentifier: "castCell")
         
         personViewModel = PersonViewModel(personId: personId)
         callToViewModelForUIUpdate()
@@ -87,8 +124,30 @@ class PersonDetailViewController: UIViewController {
         }
         
         personViewModel.movieCredits.bind { [weak self] movieCredits in
-            // update movie credits
+            self?.castDataSource = CollectionViewDataSource(cellIdentifier: "castCell", items: movieCredits ?? [], configureCell: { (cell, cast) in
+                cell.nameLabel.text = cast.originalTitle
+                cell.roleLabel.text = cast.character
+            })
+            
+            DispatchQueue.main.async {
+                self?.moviesCollectionView.dataSource = self?.castDataSource
+                self?.moviesCollectionView.reloadData()
+            }
         }
     }
-    
+}
+
+extension PersonDetailViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        var movies = personViewModel.movieCredits.value
+        if indexPath.row < movies?.count ?? 0 {
+            let movie = movies?[indexPath.row]
+            guard let movieId = movie?.id else {
+                return
+            }
+            var movieDetailVC = MovieDetailViewController()
+            // TODO: there is only id for movie we need getmovie api
+            
+        }
+    }
 }
