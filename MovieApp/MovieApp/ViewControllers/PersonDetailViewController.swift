12d969b6db8cc95e02eb78dd7caf614566442b72
yoktanela <elanuryoktan@gmail.com>
2021-07-21 05:00:30 +0300

    Set profile picture in person detail page

--- MovieApp/MovieApp/ViewControllers/PersonDetailViewController.swift
+++ MovieApp/MovieApp/ViewControllers/PersonDetailViewController.swift
@@ -7,22 +7,41 @@
 
 import Foundation
 import UIKit
+import Kingfisher
 
 class PersonDetailViewController: UIViewController {
     
     var personId: Int!
     var personViewModel: PersonViewModel!
     
+    var profileImageView: UIImageView = {
+        let imageview = UIImageView()
+        imageview.contentMode = .scaleAspectFill
+        return imageview
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.white
+        
+        self.view.addSubview(profileImageView)
+        profileImageView.translatesAutoresizingMaskIntoConstraints = false
+        let profileTop = profileImageView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 20)
+        let profileLeft = profileImageView.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 16)
+        let profileWidth = profileImageView.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.35)
+        let profileHeight = profileImageView.heightAnchor.constraint(equalTo: profileImageView.widthAnchor, multiplier: 1.5)
+        self.view.addConstraints([profileTop, profileLeft, profileWidth, profileHeight])
+        
         personViewModel = PersonViewModel(personId: personId)
         callToViewModelForUIUpdate()
     }
     
     func callToViewModelForUIUpdate() {
         personViewModel.profilePath.bind { [weak self] profilePath in
-            // set image to image view
+            if let profilePath = profilePath, let url = URL(string: Constants.imageBaseURL + profilePath) {
+                let resource = ImageResource(downloadURL: url)
+                self?.profileImageView.kf.setImage(with: resource)
+            }
         }
            
         personViewModel.name.bind { [weak self] name in
